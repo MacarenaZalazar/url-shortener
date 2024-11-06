3675d756ae5bb355794c0a1fe93390fdc52b543d
Macarena <macarena.zalazar@gmail.com>
2024-11-06 17:59:51 -0400

    mejoras en manejo de errores

--- src/controllers/urlController.ts
+++ src/controllers/urlController.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const CACHE_TTL = 3600; // 1 hora en segundos
-
+const BASE_URL = process.env.BASE_URL;
 // Genera una URL corta y la guarda en MongoDB y Redis
 export const generateShortUrl = async (
   req: Request,
@@ -17,24 +17,28 @@ export const generateShortUrl = async (
 
   const uid = new ShortUniqueId({ length: 8 }); // Define la longitud del ID
   const id = uid.randomUUID();
-  const shortUrl = `${process.env.BASE_URL}/${id}`;
+  const shortUrl = `${BASE_URL}/${id}`;
 
   // Guarda en MongoDB
   try {
     const url = new Url({ originalUrl, shortUrl, enabled: true, hits: 0 });
     await url.save();
+
+    // Almacena en caché de Redis
+    await setCache(
+      id,
+      { originalUrl, shortUrl, enabled: true, hits: 0 },
+      CACHE_TTL
+    );
+
+    res.json({ shortUrl, id });
   } catch (error) {
-    res.json(error);
+    res
+      .status(500)
+      .json({
+        error: 'Hubo un problema generando la URL corta. Inténtalo de nuevo.',
+      });
   }
-
-  // Almacena en caché de Redis
-  await setCache(
-    id,
-    { originalUrl, shortUrl, enabled: true, hits: 0 },
-    CACHE_TTL
-  );
-
-  res.json({ shortUrl, id });
 };
 
 // Redirige a la URL original
@@ -43,41 +47,44 @@ export const redirectUrl = async (
   res: Response
 ): Promise<void> => {
   const { id } = req.params;
+  try {
+    // Intenta obtener la URL desde el caché de Redis
+    let data = await getCache(id);
+    if (!data) {
+      // Si no está en caché, busca en MongoDB
+      const url = await Url.findOne({
+        shortUrl: `${BASE_URL}/${id}`,
+        enabled: true,
+      });
+      if (!url) {
+        res.status(404).json({ error: 'URL no encontrada o deshabilitada' });
+        return;
+      }
 
-  // Intenta obtener la URL desde el caché de Redis
-  let data = await getCache(id);
-  if (!data) {
-    // Si no está en caché, busca en MongoDB
-    const url = await Url.findOne({
-      shortUrl: `http://localhost:3000/${id}`,
-      enabled: true,
-    });
-    if (!url) {
-      res.status(404).json({ error: 'URL no encontrada o deshabilitada' });
-      return;
+      // Configura en el caché de Redis y actualiza el objeto `data`
+      data = {
+        originalUrl: url.originalUrl,
+        shortUrl: url.shortUrl,
+        enabled: url.enabled,
+        hits: url.hits,
+      };
+      await setCache(id, data, CACHE_TTL);
     }
 
-    // Configura en el caché de Redis y actualiza el objeto `data`
-    data = {
-      originalUrl: url.originalUrl,
-      shortUrl: url.shortUrl,
-      enabled: url.enabled,
-      hits: url.hits,
-    };
+    // Incrementa el contador de hits en Redis
+    data.hits += 1;
     await setCache(id, data, CACHE_TTL);
+
+    // Incrementa los hits en MongoDB de manera asíncrona
+    Url.updateOne(
+      { shortUrl: data.shortUrl },
+      { $inc: { hits: 1 }, $set: { lastAccessed: new Date() } }
+    ).exec();
+
+    res.redirect(data.originalUrl);
+  } catch (error) {
+    res.status(500).json({ error: 'Hubo un problema redirigiendo la URL.' });
   }
-
-  // Incrementa el contador de hits en Redis
-  data.hits += 1;
-  await setCache(id, data, CACHE_TTL);
-
-  // Incrementa los hits en MongoDB de manera asíncrona
-  Url.updateOne(
-    { shortUrl: data.shortUrl },
-    { $inc: { hits: 1 }, $set: { lastAccessed: new Date() } }
-  ).exec();
-
-  res.redirect(data.originalUrl);
 };
 
 // Actualiza el estado (habilitar/deshabilitar) de la URL
@@ -87,28 +94,36 @@ export const updateUrlStatus = async (
 ): Promise<void> => {
   const { id } = req.params;
   const { enabled } = req.body;
+  try {
+    // Actualiza en MongoDB
+    const url = await Url.findOneAndUpdate(
+      { shortUrl: `${BASE_URL}/${id}` },
+      { enabled },
+      { new: true }
+    );
+    if (!url) {
+      res.status(404).json({ error: 'URL no encontrada' });
+      return;
+    }
 
-  // Actualiza en MongoDB
-  const url = await Url.findOneAndUpdate(
-    { shortUrl: `http://localhost:3000/${id}` },
-    { enabled },
-    { new: true }
-  );
-  if (!url) {
-    res.status(404).json({ error: 'URL no encontrada' });
-    return;
+    // Actualiza en Redis si existe en el caché
+    let data = await getCache(id);
+    if (data) {
+      data.enabled = enabled;
+      await setCache(id, data, CACHE_TTL);
+    }
+
+    res.json({
+      message: `URL ${enabled ? 'habilitada' : 'deshabilitada'} correctamente.`,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error:
+          'Ocurrió un problema inesperado. Por favor, inténtalo más tarde.',
+      });
   }
-
-  // Actualiza en Redis si existe en el caché
-  let data = await getCache(id);
-  if (data) {
-    data.enabled = enabled;
-    await setCache(id, data, CACHE_TTL);
-  }
-
-  res.json({
-    message: `URL ${enabled ? 'habilitada' : 'deshabilitada'} correctamente.`,
-  });
 };
 
 // Modifica la URL de destino
@@ -118,26 +133,34 @@ export const updateOriginalUrl = async (
 ): Promise<void> => {
   const { id } = req.params;
   const { originalUrl } = req.body;
+  try {
+    // Actualiza en MongoDB
+    const url = await Url.findOneAndUpdate(
+      { shortUrl: `${BASE_URL}/${id}` },
+      { originalUrl },
+      { new: true }
+    );
+    if (!url) {
+      res.status(404).json({ error: 'URL no encontrada' });
+      return;
+    }
 
-  // Actualiza en MongoDB
-  const url = await Url.findOneAndUpdate(
-    { shortUrl: `http://localhost:3000/${id}` },
-    { originalUrl },
-    { new: true }
-  );
-  if (!url) {
-    res.status(404).json({ error: 'URL no encontrada' });
-    return;
+    // Actualiza en Redis si está en el caché
+    let data = await getCache(id);
+    if (data) {
+      data.originalUrl = originalUrl;
+      await setCache(id, data, CACHE_TTL);
+    }
+
+    res.json({ message: 'URL actualizada correctamente.' });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error:
+          'Ocurrió un problema inesperado. Por favor, inténtalo más tarde.',
+      });
   }
-
-  // Actualiza en Redis si está en el caché
-  let data = await getCache(id);
-  if (data) {
-    data.originalUrl = originalUrl;
-    await setCache(id, data, CACHE_TTL);
-  }
-
-  res.json({ message: 'URL actualizada correctamente.' });
 };
 
 // Obtiene las estadísticas de la URL
@@ -146,32 +169,40 @@ export const getUrlStats = async (
   res: Response
 ): Promise<void> => {
   const { id } = req.params;
+  try {
+    // Intenta obtener los datos del caché de Redis
+    let data = await getCache(id);
+    if (!data) {
+      // Si no está en caché, busca en MongoDB
+      const url = await Url.findOne({ shortUrl: `${BASE_URL}/${id}` });
+      if (!url) {
+        res.status(404).json({ error: 'URL no encontrada' });
+        return;
+      }
 
-  // Intenta obtener los datos del caché de Redis
-  let data = await getCache(id);
-  if (!data) {
-    // Si no está en caché, busca en MongoDB
-    const url = await Url.findOne({ shortUrl: `http://localhost:3000/${id}` });
-    if (!url) {
-      res.status(404).json({ error: 'URL no encontrada' });
-      return;
+      // Guarda en el caché y actualiza `data`
+      data = {
+        originalUrl: url.originalUrl,
+        shortUrl: url.shortUrl,
+        enabled: url.enabled,
+        hits: url.hits,
+      };
+      await setCache(id, data, CACHE_TTL);
     }
 
-    // Guarda en el caché y actualiza `data`
-    data = {
-      originalUrl: url.originalUrl,
-      shortUrl: url.shortUrl,
-      enabled: url.enabled,
-      hits: url.hits,
-    };
-    await setCache(id, data, CACHE_TTL);
+    res.json({
+      id,
+      originalUrl: data.originalUrl,
+      shortUrl: data.shortUrl,
+      enabled: data.enabled,
+      hits: data.hits,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error:
+          'Ocurrió un problema inesperado. Por favor, inténtalo más tarde.',
+      });
   }
-
-  res.json({
-    id,
-    originalUrl: data.originalUrl,
-    shortUrl: data.shortUrl,
-    enabled: data.enabled,
-    hits: data.hits,
-  });
 };
